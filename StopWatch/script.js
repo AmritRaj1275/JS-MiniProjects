619b199a505970e2b9fe74f3924de6dbe10fb266
AmritRaj1275 <amritraj04329@gmail.com>
2026-01-18 20:12:18 +0530

    completed StopWatch project

--- StopWatch/script.js
+++ StopWatch/script.js
@@ -1,3 +1,32 @@
 let secondElapsed = 0;
 let interval = null;
 const time = document.getElementById("time");
+
+
+function pad(value){
+    return String(value).padStart(2,"0")
+}
+
+function setTime(){
+    const minutes=Math.floor(secondElapsed/60)
+    const seconds=secondElapsed % 60
+    time.innerHTML=`${pad(minutes)}:${pad(seconds)}`
+}
+
+function timer(){
+    secondElapsed++;
+    setTime()
+}
+
+function startClock(){
+    if(interval) stopClock()
+   interval = setInterval(timer,1000)
+}
+function stopClock(){
+    clearInterval(interval)
+}
+function resetClock(){
+    stopClock()
+    secondElapsed=0
+    setTime()
+}
